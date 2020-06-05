2443716beee5afb42d97f54d3b4ed4f06b94e430
Jongkook <conquerex@gmail.com>
2020-06-06 02:16:35 +0900

    part7_19a : main and layout

---
+++ AndroidLab/part7_19a/src/main/java/com/example/part7_19a/Lab19_2Activity.java
@@ -0,0 +1,13 @@
+package com.example.part7_19a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab19_2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab19_2);
+    }
+}
---
+++ AndroidLab/part7_19a/src/main/java/com/example/part7_19a/MyReceiver.java
@@ -0,0 +1,4 @@
+package com.example.part7_19a;
+
+public class MyReceiver {
+}
