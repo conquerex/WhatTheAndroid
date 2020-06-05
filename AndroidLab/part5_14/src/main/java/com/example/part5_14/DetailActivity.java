fec9c3f819d527641832c9afebad0d741516f3de
Jongkook <conquerex@gmail.com>
2020-06-05 13:42:01 +0900

    init

--- AndroidLab/part5_14/src/main/java/com/example/part5_14/DetailActivity.java
+++ AndroidLab/part5_14/src/main/java/com/example/part5_14/DetailActivity.java
@@ -3,8 +3,8 @@ package com.example.part5_14;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +12,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
 /**
  * Created by kkang
  * 깡샘의 안드로이드 프로그래밍 - 루비페이퍼
@@ -21,34 +22,35 @@ public class DetailActivity extends AppCompatActivity implements AdapterView.OnI
     ListView listView;
     ArrayAdapter<String> adapter;
     ArrayList<String> datas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        Intent intent=getIntent();
-        String category=intent.getStringExtra("category");
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("category");
 
-        listView=findViewById(R.id.detail_list);
+        listView = findViewById(R.id.detail_list);
         listView.setOnItemClickListener(this);
 
-        DBHelper helper=new DBHelper(this);
-        SQLiteDatabase db=helper.getWritableDatabase();
-        Cursor cursor=db.rawQuery("select location from tb_data where category=?", new String[]{category});
-        datas=new ArrayList<>();
-        while (cursor.moveToNext()){
+        DBHelper helper = new DBHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select location from tb_data where category=?", new String[]{category});
+        datas = new ArrayList<>();
+        while (cursor.moveToNext()) {
             datas.add(cursor.getString(0));
         }
         db.close();
 
-        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datas);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datas);
         listView.setAdapter(adapter);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        TextView textView=(TextView)view;
-        Intent intent=getIntent();
+        TextView textView = (TextView) view;
+        Intent intent = getIntent();
         intent.putExtra("location", textView.getText().toString());
         setResult(RESULT_OK, intent);
         finish();
